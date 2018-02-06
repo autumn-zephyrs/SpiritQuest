ded9fb19602fd17d1205ea225bd74f75ca9944ad
toasty333 <brendanisthebest@gmail.com>
2018-02-06 12:32:16 +0000

    Boop

--- SQ/Game1.cs
+++ SQ/Game1.cs
@@ -56,6 +56,7 @@ namespace SQ
         {
 
             ScreenManager.Instance().Update(gameTime, camera2D);
+
             base.Update(gameTime);
         }
 
--- SQ/Interactable.cs
+++ SQ/Interactable.cs
@@ -9,6 +9,15 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SQ
 {
+    // Class will be for Interactable items such as loot, doors, switches and NPCs.
+
+    // Interactable items will have two properties: a tag identifying that they are interactable, and a subtype. 
+    // The subtype will denote whether an item is an item, door, switch, NPC or other interactable.
+    // All interactables will have an 'Activate' code, which will provide functionality when executed.
+    // When left clicking on interactables, left click will tell the interactable to activate and
+    // all functionality will be present within the subtype of the interactable.
+
+       
     class Interactable
     {
     }
